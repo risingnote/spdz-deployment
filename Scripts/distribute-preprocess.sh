a89623d1cd1406e9b672b9d8b3fc4944be280e08
Jonathan Evans <jon.evans@bristol.ac.uk>
2017-03-21 14:57:30 +0000

    Working on Digital Ocean

--- Scripts/distribute-preprocess.sh
+++ Scripts/distribute-preprocess.sh
@@ -6,11 +6,17 @@ mkdir Player-Data
 
 docker run -t --rm -v $(pwd)/Player-Data:/home/spdzuser/spdz/Player-Data spdz/spdz-preprocess 3
 
-ssh root@spdzalice "mkdir -p /root/spdz/Player-Data/3-128-40 && chmod -R 777 /root/spdz"
-scp -r Player-Data/*/*-P0 root@spdzalice:/root/spdz/Player-Data/3-128-40
+ssh root@spdzalice "mkdir -p /root/spdz/Player-Data/3-128-40 && mkdir -p /root/spdz/logs && chmod -R 777 /root/spdz"
+scp -r Player-Data/3-128-40/*-P0 root@spdzalice:/root/spdz/Player-Data/3-128-40
+scp -r Player-Data/3-128-40/Inputs*P0* root@spdzalice:/root/spdz/Player-Data/3-128-40
+scp -r Player-Data/3-128-40/Params-Data root@spdzalice:/root/spdz/Player-Data/3-128-40
 
-ssh root@spdzbob "mkdir -p /root/spdz/Player-Data/3-128-40 && chmod -R 777 /root/spdz"
-scp -r Player-Data/*/*-P1 root@spdzbob:/root/spdz/Player-Data/3-128-40
+ssh root@spdzbob "mkdir -p /root/spdz/Player-Data/3-128-40 && mkdir -p /root/spdz/logs && chmod -R 777 /root/spdz"
+scp -r Player-Data/3-128-40/*-P1 root@spdzbob:/root/spdz/Player-Data/3-128-40
+scp -r Player-Data/3-128-40/Inputs*P1* root@spdzbob:/root/spdz/Player-Data/3-128-40
+scp -r Player-Data/3-128-40/Params-Data root@spdzbob:/root/spdz/Player-Data/3-128-40
 
-ssh root@spdzcarol "mkdir -p /root/spdz/Player-Data/3-128-40 && chmod -R 777 /root/spdz"
-scp -r Player-Data/*/*-P2 root@spdzcarol:/root/spdz/Player-Data/3-128-40
+ssh root@spdzcarol "mkdir -p /root/spdz/Player-Data/3-128-40 && mkdir -p /root/spdz/logs && chmod -R 777 /root/spdz"
+scp -r Player-Data/3-128-40/*-P2 root@spdzcarol:/root/spdz/Player-Data/3-128-40
+scp -r Player-Data/3-128-40/Inputs*P2* root@spdzcarol:/root/spdz/Player-Data/3-128-40
+scp -r Player-Data/3-128-40/Params-Data root@spdzcarol:/root/spdz/Player-Data/3-128-40
--- Scripts/run-spdz.sh
+++ Scripts/run-spdz.sh
@@ -14,13 +14,25 @@
 #/etc/hosts mappings to substitute without escaped quote messing up docker cmd - wtf
 hostMappings='--add-host "spdz-startup spdz-party-0 spdz-party-1":192.168.1.80'
 
+#############################################################################
+# Running on 1 host
+#############################################################################
 # Container for startup coordination 
 docker run -d --name spdz-startup --rm -p 12999:12999 --env parties=2 spdz/spdz-startup
 
-# Container for player 0
-# Using IP means hosts table does not need setting up in container. 
-# When running on single host use host ip address or docker bridge network ip for startupHost
-docker run -d --rm --name workshop-voting-0 --expose 14000 -p 14000:14000 --expose 13000 -p 13000:13000 --env playerId=0 --env startupHost=192.168.1.80 -v /Users/je0018/Development/spdz/Player-Data:/home/spdzuser/spdz/Player-Data -v $(pwd)/logs:/home/spdzuser/spdz/logs spdz/workshop-voting
+# When running on single host use host ip address or docker bridge network ip (172.17.0.2) for startupHost.
+# Server.x collects docker bridge network ips 172.17.0.0)
+docker run -d --rm --name workshop-voting-0 --expose 14000 -p 14000:14000 --expose 13000 -p 13000:13000 --env playerId=0 --env startupHost=172.21.114.252 -v /Users/je0018/Development/spdz/Player-Data:/home/spdzuser/spdz/Player-Data -v $(pwd)/logs:/home/spdzuser/spdz/logs spdz/workshop-voting
 
-# Container for player 1
-docker run -d --rm --name workshop-voting-1 --expose 14001 -p 14001:14001 --expose 13001 -p 13001:13001 --env playerId=1 --env startupHost=192.168.1.80 -v /Users/je0018/Development/spdz/Player-Data:/home/spdzuser/spdz/Player-Data -v $(pwd)/logs:/home/spdzuser/spdz/logs spdz/workshop-voting
+docker run -d --rm --name workshop-voting-1 --expose 14001 -p 14001:14001 --expose 13001 -p 13001:13001 --env playerId=1 --env startupHost=172.21.114.252 -v /Users/je0018/Development/spdz/Player-Data:/home/spdzuser/spdz/Player-Data -v $(pwd)/logs:/home/spdzuser/spdz/logs spdz/workshop-voting
+
+#############################################################################
+# Running on Digital Ocean droplets
+#############################################################################
+docker run -d --name spdz-startup --rm -p 12999:12999 --env parties=3 spdz/spdz-startup:privateclient_2b1cb7
+
+docker run -d --rm --name workshop-voting-0 --expose 14000 -p 14000:14000 --expose 13000 -p 13000:13000 --env playerId=0 --env startupHost=10.131.40.47 -v /root/spdz/Player-Data:/home/spdzuser/spdz/Player-Data -v /root/spdz/logs:/home/spdzuser/spdz/logs spdz/workshop-voting:privateclient_2b1cb7
+
+docker run -d --rm --name workshop-voting-1 --expose 14001 -p 14001:14001 --expose 13001 -p 13001:13001 --env playerId=1 --env startupHost=10.131.40.47 -v /root/spdz/Player-Data:/home/spdzuser/spdz/Player-Data -v /root/spdz/logs:/home/spdzuser/spdz/logs spdz/workshop-voting:privateclient_2b1cb7
+
+docker run -d --rm --name workshop-voting-2 --expose 14002 -p 14002:14002 --expose 13002 -p 13002:13002 --env playerId=2 --env startupHost=10.131.40.47 -v /root/spdz/Player-Data:/home/spdzuser/spdz/Player-Data -v /root/spdz/logs:/home/spdzuser/spdz/logs spdz/workshop-voting:privateclient_2b1cb7
