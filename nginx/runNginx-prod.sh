a5ba70e8d585a03b56684d085e4514d069a82ad9
Jonathan Evans <jon.evans@bristol.ac.uk>
2017-05-12 17:06:12 +0100

    Start nginx deployment.

--- nginx/runNginx-prod.sh
+++ nginx/runNginx-prod.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Run nginx reverse proxy.
+# Note that proxy_pass will be sending forwards out with docker bridge network IPs (172.17.0), if firewall needs openning.
 
-docker stop nginx-proxy
-
-docker run --name nginx-proxy --rm -d -p 443:443 -v /opt/spdz/nginx/exampleProxy.conf:/etc/nginx/nginx.conf:ro -v /opt/spdz/logs:/var/log/nginx -v /opt/spdz/certs:/etc/certs nginx:1.12.0
+docker run --name nginx-proxy --rm -d -p 443:443 -v /opt/spdz/nginx/nginx.conf:/etc/nginx/nginx.conf:ro -v /opt/spdz/logs:/var/log/nginx -v /opt/spdz/certs:/etc/certs nginx:1.12.0
